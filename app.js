095a4f395dabfb8a4f525f3c634e3bca0f4cf623
horii203 <89522294+horii203@users.noreply.github.com>
2024-03-25 12:36:30 +0900

    型について

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// : number これは数値ですという意味
 function add(n1, n2) {
     return n1 + n2;
 }
@@ -5,3 +6,5 @@ var number1 = '5';
 var number2 = 2.8;
 var result = add(number1, number2);
 console.log(result);
+var test = 5; // 型推論されるので:numberは不要
+test = 'hello'; // 文字列を入れるとエラーになる
